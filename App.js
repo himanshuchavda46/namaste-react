acee99f1274898d0d198b83ad281140f0436ec90
himanshuchavda44 <89018688+himanshuchavda44@users.noreply.github.com>
2023-04-11 17:20:21 +0530

    feat: add episode 1(react setup)

---
+++ App.js
@@ -0,0 +1,21 @@
+const heading = React.createElement(
+    "h1", // type of tag
+    {id: "heading",xyz: "abc"}, // attributes
+    "Hello World from React" // childrens
+);
+
+console.log(heading);
+// react element is nothing but normal js objects
+// props are attributes + children
+
+//nested tags and siblings in react
+const children1 = React.createElement("div",{id: "children1"},"child1")
+const children2 = React.createElement("div",{id: "children2"},"child2")
+const parent = React.createElement("div",{id: "parent"},[children1,children2]);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+// render method basically convert this object to html tags
+// ReactElement(Object) => HTML(Browser Understands)
+// if let say there is something inside of html tag with root id but root.render will replace those things
